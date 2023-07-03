603b4907482c845a659ecfec503c5eadc677ead0
xiaoshu <2376628285@qq.com>
2023-07-03 16:53:25 +0800

    测试新配置的ssh文件

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -25,6 +25,7 @@ export default {
     }
   },
   created() {
+    console.log('测试')
     var resizeTimer = null;
     window.addEventListener('resize', function () {
       let clientWidth = document.documentElement.clientWidth

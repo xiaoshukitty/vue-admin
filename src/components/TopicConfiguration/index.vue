d7efc094b7b2cbff2c44471cb6321ea4f56651a0
xiaoshu <2376628285@qq.com>
2024-09-26 21:57:49 +0800

    fix(TopicConfiguration):增加内置主题

--- src/components/TopicConfiguration/index.vue
+++ src/components/TopicConfiguration/index.vue
@@ -2,13 +2,38 @@
     <div class="TopicConfiguration">
         <el-drawer title="主题配置" :visible.sync="themeSwitch" :direction="direction">
             <div class="content">
-                <div class="content-list">
-                    <div class="content-item" v-for="(item, index) in themelist" :key="index">
-                        <div class="content-item-title">{{ item.themeName }}</div>
-                        <div class="content-item-switch">
-                            <el-switch v-model="item.switchValue" active-color="#41b584" inactive-color="#dcdfe6"
-                                @change="switchChange(item)">
-                            </el-switch>
+                <div class="tab-border">
+                    <ul class="tab-tilte">
+                        <li v-for="(item, index) in tabs" :key="index" :class="{ active: tabIndex == index }"
+                            @click="changeTab(index)">
+                            <div>{{ item }}</div>
+                        </li>
+                    </ul>
+                </div>
+                <div>
+                    <div class="content-list" v-if="tabIndex == 1">
+                        <div class="content-item" v-for="(item, index) in themelist" :key="index">
+                            <div class="content-item-title">{{ item.themeName }}</div>
+                            <div class="content-item-switch">
+                                <el-switch v-model="item.switchValue" active-color="#41b584" inactive-color="#dcdfe6"
+                                    @change="switchChange(item)">
+                                </el-switch>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="content-list" v-if="tabIndex == 0">
+                        <div class="built-theme">
+                            <h3>内置主题</h3>
+                            <div class="theme-color">
+                                <div class="theme-list" v-for="(item, index) in themeColor" :key="index">
+                                    <div class="theme-item"
+                                        :style="{ border: colorIndex == index ? '1px solid' + ' ' +item.color : '' }"
+                                        @click="colorChange(index)">
+                                        <div class="theme-item-box" :style="{ background: item.color }"></div>
+                                    </div>
+                                    <div class="theme-text">{{ item.name }}</div>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
@@ -81,7 +106,27 @@ export default {
                 themeName: '全屏',
                 switchValue: false,
                 refValue: 'fullScreenGuide',
-            }]
+            }],
+            tabs: ['外观', '布局', '功能'],
+            tabIndex: 0,
+            themeColor: [
+                { name: '默认', color: '#006be6' },
+                { name: '紫罗兰', color: 'rgb(113, 102, 240)' },
+                { name: '樱花粉', color: 'rgb(232, 74, 108)' },
+                { name: '柠檬黄', color: 'rgb(239, 189, 72)' },
+                { name: '天蓝色', color: 'rgb(78, 105, 253)' },
+                { name: '浅绿色', color: 'rgb(11, 208, 146)' },
+                { name: '锌色灰', color: 'rgb(63, 63, 70)' },
+                { name: '深绿色', color: 'rgb(13, 148, 150)' },
+                { name: '深蓝色', color: 'rgb(9, 96, 190)' },
+                { name: '橙黄色', color: 'rgb(193, 66, 11)' },
+                { name: '玫瑰红', color: 'rgb(187, 27, 27)' },
+                { name: '中性色', color: 'rgb(64, 64, 64)' },
+                { name: '石板灰', color: 'rgb(52, 66, 86)' },
+                { name: '中灰色', color: 'rgb(56, 66, 82)' },
+                { name: '自定义', color: 'rgb(223, 223, 223)' },
+            ],
+            colorIndex: 0,//当前选中颜色
         }
     },
     mounted() {
@@ -117,6 +162,14 @@ export default {
         //关闭
         save() {
             this.$emit('update:drawer', false)
+        },
+        //切换tab
+        changeTab(ind) {
+            this.tabIndex = ind;
+        },
+        //切换主题颜色
+        colorChange(ind) {
+            this.colorIndex = ind;
         }
     }
 }
@@ -125,32 +178,100 @@ export default {
 <style lang="less" scoped>
 .TopicConfiguration {
     .content {
-        padding: 20px;
+        padding: 1.25rem;
 
         .content-list {
+            margin-top: .625rem;
+
             .content-item {
                 display: flex;
                 align-items: center;
                 justify-content: space-between;
-                height: 50px;
+                height: 3.125rem;
 
                 .content-item-title {
-                    font-size: 14px;
+                    font-size: .875rem;
                     color: #333;
                 }
 
                 .content-item-switch {
                     ::v-deep .el-switch__core {
-                        width: 40px !important;
-                        height: 20px;
-                        border-radius: 10px;
+                        width: 2.5rem !important;
+                        height: 1.25rem;
+                        border-radius: .625rem;
                     }
 
                     ::v-deep .el-switch__core::after {
-                        width: 16px;
-                        height: 16px;
+                        width: 1rem;
+                        height: 1rem;
                     }
                 }
+            }
+
+            .built-theme {
+                .theme-color {
+                    display: grid;
+                    grid-template-columns: repeat(5, 1fr);
+                    column-gap: .5rem;
+                    row-gap: .5rem;
+                    cursor: pointer;
+
+                    .theme-list {
+                        .theme-item {
+                            padding: .8125rem 1.25rem;
+                            border: .0625rem solid #e4e4e7;
+                            border-radius: .625rem;
+
+                            .theme-item-box {
+                                width: 1.25rem;
+                                height: 1.25rem;
+                                border-radius: .3125rem;
+                                margin: auto;
+                            }
+                        }
+                    }
+
+                    .theme-text {
+                        text-align: center;
+                        margin: .625rem 0;
+                    }
+                }
+            }
+        }
+
+        .tab-border {
+            background-color: #f4f4f5;
+            padding: .3125rem;
+            border-radius: .625rem;
+
+            ul li {
+                margin: 0;
+                padding: 0;
+                list-style: none;
+            }
+
+            .tab-tilte {
+                display: flex;
+            }
+
+            .tab-tilte li {
+                flex: 1;
+                padding: .5rem;
+                text-align: center;
+                background-color: #f4f4f5;
+                border-radius: .625rem;
+                cursor: pointer;
+                transition: background-color 0.3s;
+                color: #71717a;
+                /* 添加过渡效果 */
+
+            }
+
+            /* 点击对应的标题添加对应的背景颜色 */
+            .tab-tilte .active {
+                background-color: #fff;
+                color: #71717a;
+                border-radius: .625rem;
             }
         }
     }
@@ -160,16 +281,16 @@ export default {
         bottom: 0;
         left: 0;
         width: 100%;
-        height: 70px;
+        height: 4.375rem;
         background: #fff;
-        border-top: 1px solid #ccc;
+        border-top: .0625rem solid #ccc;
 
         .footer-btn {
-            height: 70px;
+            height: 4.375rem;
             display: flex;
             align-items: center;
             justify-content: end;
-            padding: 0 20px;
+            padding: 0 1.25rem;
 
             ::v-deep .el-button--success {
                 background-color: #41b584 !important;
@@ -177,5 +298,9 @@ export default {
             }
         }
     }
+}
+
+::v-deep .el-drawer__body {
+    border-top: .0625rem solid #ccc;
 }
 </style>
